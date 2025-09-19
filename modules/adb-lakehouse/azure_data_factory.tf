6459d98ac81526e3db72c8c2bbf6245426b0428b
Mario Jiménez Gárate <156453484+mario-jimenez-g@users.noreply.github.com>
2025-09-19 18:45:05 -0400

    Delete modules/adb-lakehouse directory

--- modules/adb-lakehouse/azure_data_factory.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_data_factory" "adf" {
-  count = var.data_factory_name != "" ? 1 : 0
-
-  name                = var.data_factory_name
-  location            = local.rg_location
-  resource_group_name = local.rg_name
-  tags                = var.tags
-}
