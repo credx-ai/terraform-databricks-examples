22554c9ec0ea7a9480a46f544a1330b443315969
Mario Jiménez Gárate <156453484+mario-jimenez-g@users.noreply.github.com>
2025-09-19 19:11:40 -0400

    Delete examples/aws-workspace-basic directory

--- examples/aws-workspace-basic/main.tf
+++
@@ -1,8 +0,0 @@
-module "aws-workspace-basic" {
-  source                = "github.com/databricks/terraform-databricks-examples/modules/aws-workspace-basic"
-  databricks_account_id = var.databricks_account_id
-  region                = var.region
-  tags                  = var.tags
-  prefix                = var.prefix
-  cidr_block            = var.cidr_block
-}
